47b86e2796df845a4acc430bb77ef3b6b824cdb0
Jenny Schweers <orange.jenny@gmail.com>
2014-03-22 20:14:42 -0400

    first url

--- vegan/urls.py
+++ vegan/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+import vegan.views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'vegan.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    (r'.*', vegan.views.index),
     url(r'^admin/', include(admin.site.urls)),
 )
